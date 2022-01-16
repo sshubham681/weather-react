2125ea3f9b99864370cba82ec7e8e478ab64d791
Shubham Verma <77584464+sshubham681@users.noreply.github.com>
2022-01-16 22:58:06 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import Temp from "./components/weather/Temp";
+function App() {
+  return (
+    <div className="App">
+      <Temp />
+    </div>
+  );
+}
+
+export default App;
